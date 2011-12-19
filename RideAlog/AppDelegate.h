41a3438b56ca1f6a5d7d0aaf78c9ac2de9139740
Andrew Latimer <andrew@elpasoera.com>
2011-12-19 13:20:21 -0800

    Initial Commit

---
+++ RideAlog/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RideAlog
+//
+//  Created by Andrew Latimer on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
